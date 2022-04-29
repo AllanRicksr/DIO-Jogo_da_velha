b62c0987090133d37d7564120b9fc4506a49e174
AllanRicksr <allanrickrodrigues@hotmail.com>
2022-04-29 15:47:35 -0400

    Adicionando segundo jogador

--- jogoV.js
+++ jogoV.js
@@ -1,16 +1,40 @@
 var jogador = null;
 var jogadorSelecionado = document.getElementById('jogador-da-vez');
+var quadrados = document.getElementsByClassName('quadrado');
 
 mudarJogador('X');
 
 function acaoDoQuadrado(id) {
     var quadrado = document.getElementById(id);
+    if (quadrado.innerHTML !== '-') {
+        return
+    }
 
     quadrado.innerHTML = jogador;
     quadrado.style.color = '#000';
+
+    if (jogador === 'X') {
+        jogador = 'O';
+    } else {
+        jogador = 'X';
+    }
+
+    mudarJogador;
 }
 
 function mudarJogador(valor) {
     jogador = valor;
     jogadorSelecionado.innerHTML = jogador;
+}
+
+function checaVencedor() {
+    var quadrado1 = document.getElementById(1);
+    var quadrado2 = document.getElementById(2);
+    var quadrado3 = document.getElementById(3);
+    var quadrado4 = document.getElementById(4);
+    var quadrado5 = document.getElementById(5);
+    var quadrado6 = document.getElementById(6);
+    var quadrado7 = document.getElementById(7);
+    var quadrado8 = document.getElementById(8);
+    var quadrado9 = document.getElementById(9);
 }

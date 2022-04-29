bfa387120c6400f031c11df8f8832ae5264ed8bd
AllanRicksr <allanrickrodrigues@hotmail.com>
2022-04-29 15:37:23 -0400

    Criando os jogadores

--- jogoV.js
+++ jogoV.js
@@ -1,5 +1,16 @@
 var jogador = null;
+var jogadorSelecionado = document.getElementById('jogador-da-vez');
+
+mudarJogador('X');
 
 function acaoDoQuadrado(id) {
-    console.log(id);
+    var quadrado = document.getElementById(id);
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000';
+}
+
+function mudarJogador(valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
 }
